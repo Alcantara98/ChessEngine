a92ea13339ac755de5cfe352a661832a825210a5
Elbert Alcantara <72234631+Alcantara98@users.noreply.github.com>
2024-10-14 08:40:45 +1300

    Adding first Gtest.

--- tests/move_generator_tests.cpp
+++ tests/move_generator_tests.cpp
@@ -1,1 +1,12 @@
+#include <gtest/gtest.h>
 #include "move_generator.h"
+#include "piece.h"
+
+/**
+ *  @brief Test Pawn
+ *  
+ *  @details Pawn, no pieces infront of it, one possible move forward.
+ */
+TEST(PawnTest, NoPiecesInFront) {
+    EXPECT_EQ(generatePawnMove(1), expected_output);  // Replace with actual function and expected output
+}
